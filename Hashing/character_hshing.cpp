732b73061c0ff2d5961ea81c4405bf972252e5a2
Ashia Sultana <ashia.sultana.maisha@gmail.com>
2024-12-06 19:57:27 +0600

    Added Number_hashing.cpp to Hashing folder

---
+++ Hashing/character_hshing.cpp
@@ -0,0 +1,28 @@
+//"abcdabcgc" how many times "a" appears or some other character appers
+/*for upper case we can use 'ch' - 'A' */
+//by doing 'ch' - 'a'(substracting ASSCI value of character 'a' from other character) we can get the coresponding indax
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s;
+    cin>>s;
+
+    int hash[26] = {0};
+    for(int i = 0; i<s.size(); i++){
+        hash[s[i] - 'a']++;//for all character hash[s[i]]
+    }
+    int q;
+    cin>>q;
+    while(q--){
+        char c;
+        cin>>c;
+        //fetch
+        cout<<hash[c - 'a']<<endl; //for all character hash[c]
+
+    }
+    
+
+
+    return 0;
+}
